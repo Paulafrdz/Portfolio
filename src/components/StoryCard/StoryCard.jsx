d4d657f073c6a82056093f3884582c7e24c42e3e
Paula Fernandez Aranda <paulafa8@gmail.com>
2026-01-21 11:15:35 +1100

    logos

--- src/components/StoryCard/StoryCard.jsx
+++ src/components/StoryCard/StoryCard.jsx
@@ -1,4 +1,7 @@
 import RevealText from "/Users/Paula/Desktop/Portfolio/src/components/RevealText/RevealText.jsx";
+import email from "../../assets/email-9-svgrepo-com (2).svg"
+import github from "../../assets/github-outline-fill-svgrepo-com (1).svg"
+import linkedin from "../../assets/linkedin-svgrepo-com (3).svg"
 import "./Storycard.css";
 
 export default function StoryCard() {
@@ -11,8 +14,36 @@ export default function StoryCard() {
                         "accessible, visually refined web interfaces.",
                         "Adaptable, a collaborative team player,problem-solver, and ",
                         "passionate about crafting products that balance functionality and design. ",
-                        
-                    ]} />
+
+                    ]}
+                    
+                     />
+                <div className="story-logos">
+                    <a
+                        href="https://github.com/Paulafrdz"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label="GitHub"
+                    >
+                        <img src={github} alt="GitHub" />
+                    </a>
+
+                    <a
+                        href="https://www.linkedin.com/in/paulafernandezaranda/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label="LinkedIn"
+                    >
+                        <img src={linkedin} alt="LinkedIn" />
+                    </a>
+
+                    <a
+                        href="mailto:paulafa8@email.com"
+                        aria-label="Email"
+                    >
+                        <img src={email} alt="Email" className="logo-email"/>
+                    </a>
+                </div>
             </div>
         </section>
     );
